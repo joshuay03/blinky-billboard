23ec710c8f80fa8816ec29928d19a41c847ec0b0
Joshua Young <djry1999@gmail.com>
2020-04-11 16:09:36 +1000

    Introduce classes for login and option menu

---
+++ src/Login.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+    public static void main(String[] args) {
+        Login frame1 = new Login();
+    }
+
+    JPanel panel = new JPanel();
+    JTextField userField = new JTextField(15);
+    JPasswordField passField = new JPasswordField(15);
+    JButton loginButton = new JButton("Login");
+
+    Login() {
+        super("Login Authentication");
+        setSize(300, 200);
+        setLocation(500, 280);
+        panel.setLayout(null);
+
+        userField.setBounds(70, 30, 150, 20);
+        passField.setBounds(70, 65, 150, 20);
+        loginButton.setBounds(110, 100, 80, 20);
+
+        panel.add(userField);
+        panel.add(passField);
+        panel.add(loginButton);
+
+        getContentPane().add(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        actionLogin();
+    }
+
+    public void actionLogin() {
+        loginButton.addActionListener(ae -> {
+            String user = userField.getText();
+            String pass = passField.getText();
+            if (user.equals("josh") && pass.equals("12345")) {
+                OptionMenu regFace = new OptionMenu();
+                regFace.setVisible(true);
+                dispose();
+            } else {
+                JOptionPane.showMessageDialog(null, "Wrong Password / Username");
+                userField.setText("");
+                passField.setText("");
+                userField.requestFocus();
+            }
+        });
+    }
+
+}
---
+++ src/OptionMenu.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class OptionMenu extends JFrame {
+
+    public static void main(String[] args) {
+        OptionMenu frame2 = new OptionMenu();
+    }
+
+    JLabel welcome = new JLabel("OptionMenu Frame");
+    JPanel panel = new JPanel();
+
+    OptionMenu() {
+        super("Welcome");
+        setSize(300, 200);
+        setLocation(500, 280);
+        panel.setLayout(null);
+
+        welcome.setBounds(70, 50, 150, 60);
+
+        panel.add(welcome);
+
+        getContentPane().add(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
