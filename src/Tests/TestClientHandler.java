f53d0cea967dadd319ba40c65cae3ae12d19ab26
Nick <nick.dodig@gmail.com>
2020-04-13 13:52:51 +1000

    Version 6: updated clientHandler tests

--- src/Tests/TestClientHandler.java
+++ src/Tests/TestClientHandler.java
@@ -14,6 +14,7 @@ public class TestClientHandler {
     DataInputStream input;
     DataOutputStream output;
     Socket client;
+    ClientHandler clientHandler;
 
     /*
         Init the clientHandler object.
@@ -21,13 +22,37 @@ public class TestClientHandler {
     @BeforeAll
     public void initClientHandlerObject() {
         try {
-            Socket client = new Socket();
-            DataInputStream input = new DataInputStream(client.getInputStream());
-            DataOutputStream output = new DataOutputStream(client.getOutputStream());
-            ClientHandler clientHandler = new ClientHandler(client, input, output);
+            client = new Socket();
+            input = new DataInputStream(client.getInputStream());
+            output = new DataOutputStream(client.getOutputStream());
+            clientHandler = new ClientHandler(client, input, output);
         }
         catch(Exception e) {
             System.out.println(e);
         }
+    }
+
+    /*
+        Test sending basic output to the client.
+     */
+    @Test
+    public void sendBasicOutput() {
+        clientHandler.sendOutput();
+    }
+
+    /*
+        Test retrieving basic input from the client.
+     */
+    @Test
+    public void retrieveBasicInput() {
+        clientHandler.retrieveInput();
+    }
+
+    /*
+        Test closing the threads connection to the server.
+     */
+    @Test
+    public void closeThreadConnectionToServer() {
+        clientHandler.closeConnection();
     }
 }
