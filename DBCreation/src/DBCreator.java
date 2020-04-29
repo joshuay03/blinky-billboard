d14f5b17e54c101ca3e1848bf70bea1e88d15ab9
Cecil Hammett <cecilhammettt4@gmail.com>
2020-04-29 21:04:57 +1000

    Changed to get absolute path

--- DBCreation/src/DBCreator.java
+++ DBCreation/src/DBCreator.java
@@ -1,11 +1,14 @@
+import java.io.File;
 import java.io.IOException;
-import java.sql.SQLException;
 
 public class DBCreator {
-    public static void main(String args[]) throws SQLException {
-        Runtime rt = Runtime.getRuntime();
+    public static void main(String[] args) {
+        File file = new File("blinkybillboard.sql");
+        String path = file.getPath();
+
         try {
-            Process pr = rt.exec(new String[]{"/bin/bash", "-c", "mariaDB -ppassword -u root -h localhost testDB < ../../blinkybillboard.sql"});
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec(new String[]{"/bin/bash", "-c", "mariaDB -ppassword -u root -h localhost testDB < " + path});
             pr.waitFor();
         } catch (IOException | InterruptedException e) {
             System.out.println(e.toString());
