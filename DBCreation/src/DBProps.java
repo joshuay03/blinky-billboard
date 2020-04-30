8f64ac4f5f583104c19b7316f797ca53d5e476b9
Liran Piade <liranpiade@gmail.com>
2020-04-30 21:13:31 +1000

    Attempting to read from props file - file is found but the data is all null

---
+++ DBCreation/src/DBProps.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Properties;
+
+public class DBProps {
+    private String url;
+    private String schema;
+    private String username;
+    private String password;
+
+    DBProps() throws IOException {
+
+        FileReader propsFile = new FileReader("./db.props");
+        Properties props = new Properties();
+        props.load(propsFile);
+        this.url = props.getProperty("url");
+        this.username = props.getProperty("username");
+        this.password = props.getProperty("password");
+        this.schema = props.getProperty("schema");
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+        DBProps props = new DBProps();
+
+        System.out.println("Done");
+    }
+}
