913a0ee01637ed4d2e33ab6fc872fd12e6333781
Cecil Hammett <cecilhammettt4@gmail.com>
2020-04-30 21:50:38 +1000

    Connection to sql implemented reading sql file

--- DBCreation/src/DBCreator.java
+++ DBCreation/src/DBCreator.java
@@ -2,13 +2,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class DBCreator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("blinkybillboard.sql");
+        DBProps props = new DBProps();
         String path = file.getPath();
 
         try {
             Runtime rt = Runtime.getRuntime();
-            Process pr = rt.exec(new String[]{"/bin/bash", "-c", "mariaDB -ppassword -u root -h localhost testDB < " + path});
+            Process pr = rt.exec(new String[]{"/bin/bash", "-c", "mariaDB -p",props.password, " -u ", props.username, " -h ", props.url , " ", props.schema," < " + path});
             pr.waitFor();
         } catch (IOException | InterruptedException e) {
             System.out.println(e.toString());
