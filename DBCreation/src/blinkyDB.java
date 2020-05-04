8d5a7050c3428ae5e57c39b50549924f0ae3c4b8
liran <liranpiade@gmail.com>
2020-05-04 13:23:36 +1000

    Formed a db connection based on db.props

--- DBCreation/src/blinkyDB.java
+++ DBCreation/src/blinkyDB.java
@@ -1,11 +1,24 @@
+import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class blinkyDB {
     private DBProps props;
-    public blinkyDB() throws IOException { // Create a new database object - attempting to populate an actual database if one isn't already initialised. Otherwise, start a connection to a database.
-        props = new DBProps(); // Read db.props
-    }
-    public static void main(String[] args){
+    public Connection dbconn;
 
+    public blinkyDB() throws IOException, SQLException { // Create a new database object - attempting to populate an actual database if one isn't already initialised. Otherwise, start a connection to a database.
+        props = new DBProps(); // Read db.props
+        String sqldump = new File("blinkybillboard.sql").getPath();
+        try { // DB creation code
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec(new String[]{"/bin/bash", "-c", "mariaDB -p",props.password, " -u ", props.username, " -h ", props.url , " ", props.schema," < " + sqldump});
+            pr.waitFor();
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.toString());
+        }
+        // Start a database connection
+        dbconn = DriverManager.getConnection("jdbc:mariadb://"+props.url+":3306/"+props.schema, props.username, props.password);
     }
 }
---
+++ DBCreation/src/blinkyDBTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+class blinkyDBTest {
+    public static void main(String[] args) throws IOException, SQLException {
+        blinkyDB db = new blinkyDB();
+    }
+}
