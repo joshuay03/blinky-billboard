c4f74c993a9d8f740d6c349eb91f91f8cff76e19
zanejhetam <zane.jhetam@connect.qut.edu.au>
2020-05-07 22:06:16 +1000

    Pseudocode for authentication + implement serializable in Billboard

--- src/Server/AuthenticationHandler.java
+++ src/Server/AuthenticationHandler.java
@@ -6,6 +6,15 @@ public class AuthenticationHandler {
 
     public static boolean Authenticate (Credentials credentials){
 
+        // If the user does not exist, return false if the user does not exist.
+
+        // If the user does exist ...
+
+        // Concatenate the password and the salt
+
+        // Hash the result
+
+        // If the result of the password + salt hash matches the content of the database, return true
 
 
         return false; // Assume authentication failed if no success case reached
