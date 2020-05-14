34744f92b55a7ff0c36f50c399b69f1ce7e75a5b
Cecil Hammett <cecilhammettt4@gmail.com>
2020-05-14 11:38:01 +1000

    Login page now connects to server with some possible changes to the authentication class. Many ways to go about this , will discuss in meeting.

--- src/Client/Login.java
+++ src/Client/Login.java
@@ -1,11 +1,10 @@
 package Client;
 
-import Server.DBProps;
+import Server.*;
+import SocketCommunication.Credentials;
 
 import javax.swing.*;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -16,7 +15,7 @@ public class Login extends JFrame {
     JPasswordField passField = new JPasswordField(15);
     JButton loginButton = new JButton("Login");
 
-    Login(User user) {
+    Login() {
         super("Login");
         setSize(300, 200);
         setLocation(500, 280);
@@ -33,40 +32,34 @@ public class Login extends JFrame {
         getContentPane().add(panel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        actionLogin(user);
+
+        actionLogin();
     }
 
-    public void actionLogin(User user) {
+    public void actionLogin() {
 
         loginButton.addActionListener(ae -> {
 
-            DBProps props = null;
-            Connection dbconn = null;
-            try {
-                props = new DBProps();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
             String username = userField.getText();
             char[] password = passField.getPassword();
 
-            try {
-                dbconn = DriverManager.getConnection("jdbc:mariadb://" + props.url + ":3306/" + props.schema, props.username, props.password);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+          Credentials credentials ;
+
+
 
             try {
-                if (username != null && password != null) {
 
-                    // need to figure out how to unhash password password is currently hardcoded
-                    //need a function that hashes and unhashes passcode for create users and login 
-                    String sql = String.format("Select * from Users Where user_name= '%s' and password_hash= 'pass123'", username);
-                    ResultSet rs = dbconn.createStatement().executeQuery(sql);
-                    if (rs.next()) {
+
+                if (username!= null && password != null) {
+
+                    //only create credential object when values are not null
+
+                    credentials = new Credentials(username,password.toString());
+
+                    if (AuthenticationHandler.Authenticate(credentials)) {
                         //in this case enter when at least one result comes it means user is valid
-                        OptionMenu optionMenuPanel = new OptionMenu(user);
+                        OptionMenu optionMenuPanel = new OptionMenu(Authenticate.user);
                         optionMenuPanel.setVisible(true);
                         dispose();
                     } else {
@@ -78,7 +71,7 @@ public class Login extends JFrame {
                     }
                 }
 
-            } catch (SQLException err) {
+            } catch (IOException | SQLException err ) {
                 JOptionPane.showMessageDialog(this, err.getMessage());
             }
 
