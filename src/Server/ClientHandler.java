f74009a40aaff9e96965bf89ef633201e58f52c9
Cecil Hammett <cecilhammettt4@gmail.com>
2020-05-19 10:09:39 +1000

    Adding comments to requests in Client Handler to explain exaclty what logic needs to happen based on CRA. Hope this makes the process easy.

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -101,32 +101,85 @@ public class ClientHandler extends Thread implements SocketCommunication {
                 // The control panel send the server the Billboard Name and valid session
                 // token e.g session = req.getSession();
 
+                //server responds with billboards contents
+
                 break;
             case CREATE_BILLBOARD:
                 // triggered inside CreateBillboards() GUI
                 // Client sends the Server the billboards name, contents (billboard ID, creator) and valid session token
-                // get list of billboards session user has created if billboardName in list make edit if not return error
                 // something like String billboardName = req.getData().get(billboardName);
-                // if billboardName does not exist respond with create new billboard
-                // if billboardName does exist and is not currently scheduled respond by replacing contents of billboard with new contents
 
+                // get list of billboards session user has created
+                // if billboardName in list make edit if not return error
+
+                // if billboardName does not exist create new billboard
+                // if billboardName exist and is currently scheduled edit can not be made return error
+                // if billboardName exist and is not currently scheduled replace contents of billboard with new contents
 
                 break;
             case EDIT_BILLBOARD:
                 // this request will only happen if User has 'Edit all Billboards' permission
-                //
+                // Client sends server billboard name, contents (billboard ID, creator) and valid session token
+                // Edit can be made by this user to any billboard on list (even if currently scheduled)
+                // if edit is made replace contents of billboard with new
+
                 break;
             case DELETE_BILLBOARD:
+                // Client sends server billboard name and valid session token
+                // Do something to find or check the perrmissions of session if not already found
+
+                // if session.permissions = 'Create Billboards' get list of billboards session user has created
+                // if billboard is created by session user and not currently scheduled allow delete else return error
+
+                // if session.permissions = 'Edit All Billboards' allow delete on any billboard
+
                 break;
             case VIEW_SCHEDULED_BILLBOARD:
+                // this request will only happen is user has 'Schedule Billboards' permission
+                // I think it should be triggered inside the ScheduleBillboards() GUI
+
+                // client will send server a valid session
+                // if session token is valid server will respond with list of billboards the have been scheduled
+                // including billboardName, creator, time scheduled, and duration
+
                 break;
             case SCHEDULE_BILLBOARD:
+                // this request will only happen is user has 'Schedule Billboards' permission
+                // triggered inside the ScheduleBillboards() GUI
+
+                //Client will send server billboardName, timeScheduled, duration, and valid session
+                // (there also might be more information e.g for handling recurrence)
+
+                // Server will then respond by adding new billboard to schedule
+                // if there is a billboard previously scheduled for the time of the newly scheduled billboard
+                // new billboard will take precedence over previously scheduled billboard but will not delete previously scheduled
+
+                // e.g billboard A scheduled from 10:00-11:00 and billboard B is then scheduled from 10:30-11
+                // billboard A will display until 10:30 then billboard B will be displayed
+
+                // once billboard is scheduled Server will send back an acknowledgement of success
+
                 break;
             case REMOVE_SCHEDULED:
+                // this request will only happen is user has 'Schedule Billboards' permission
+                // triggered inside the ScheduleBillboards() GUI
+
+                //Client will send the valid session token of the user along with billboardName and scheduledTime
+                // of billboard that is to be deleted
+
+                // Server will respond by removing the billboard from the schedule and sending back an acknowledgement of success
+
                 break;
             case LIST_USERS:
+                // request only happens if user has 'Edit Users' permission
+                // triggered inside EditUsers() GUI
+
+                // Client sends server valid session token
+                // Server responds with list of usernames (CRA then says "and any other information your teams feels appropriate")
+
                 break;
             case CREATE_USER:
+
                 break;
             case GET_USER_PERMISSION:
                 break;
