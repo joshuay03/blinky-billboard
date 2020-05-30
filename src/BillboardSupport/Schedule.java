a5b2512004cb9104990039b3796156d398811ecc
Cecil Hammett <cecilhammettt4@gmail.com>
2020-05-30 16:15:12 +1000

    started schedule gui call

--- src/BillboardSupport/Schedule.java
+++ src/BillboardSupport/Schedule.java
@@ -1,14 +1,17 @@
 package BillboardSupport;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 
-public class Schedule {
+public class Schedule implements Serializable {
     public Timestamp StartTime;
     public int duration; // Minutes
     public int repeatInterval;
     public String billboardName;
 
-    public Schedule(Timestamp startTime, int duration, int repeatInterval, String billboardName) {
+    public Schedule(Timestamp startTime, int duration, int repeatInterval, String billboardName)
+
+    {
         StartTime = startTime;
         this.duration = duration;
         this.repeatInterval = repeatInterval;
